03944af077a65132622a2ff646744444fe8d9962
KaiBlaze-afk <Kumaradarshh5@gmail.com>
2025-02-04 23:23:33 +0530

    This is version 2 of the chatbot with context

--- main.py
+++ main.py
@@ -1,4 +1,19 @@
 from langchain_ollama import OllamaLLM
+from langchain_core.prompts import ChatPromptTemplate
+
+template = """
+Answer the question below.
+
+Here is the conversation history: {context}
+
+Question: {question}
+
+Answer:
+"""
+
 model = OllamaLLM(model="llama3.2")
-result = model.invoke(input="Hello how are you?")
+prompt = ChatPromptTemplate.from_template(template)
+chain = prompt | model
+
+result = chain.invoke({"context": "You need to talk like a pirate", "question":"Hello"})
 print(result)
